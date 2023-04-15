d206dda5a1f3eb5b706b0cbba3a7a27c53d3152b
Danil Mysenko <danil.mysenko@yandex.ru>
2023-04-15 14:59:00 +0300

    https протокол

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const apiConfig = {
-  url: 'http://back.mesto.nomoredomains.monster',
+  url: 'https://back.mesto.nomoredomains.monster',
   headers: {
     'Content-Type': 'application/json; charset=UTF-8'
   }
