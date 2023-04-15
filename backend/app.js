8a9c235c785ad00a86cffdcbb145c012539ce3ee
Danil Mysenko <danil.mysenko@yandex.ru>
2023-04-15 15:01:26 +0300

    Внедрить код для краш-теста сервера

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,11 @@ mongoose
 
 app.use(requestLogger);
 app.use(cors);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.use('/', router);
 app.use(errorLogger);
 
